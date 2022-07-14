400e596296902a92f17ed7bbeace34e209bf579f
valeria.golovko <valeria.golovko@megafon.ru>
2022-07-14 09:19:46 +0400

    fixed mistakes

--- src/main/java/com/example/MyBookShopApp/data/Book.java
+++ src/main/java/com/example/MyBookShopApp/data/Book.java
@@ -3,7 +3,7 @@ package com.example.MyBookShopApp.data;
 public class Book {
 
     private Integer id;
-    private String author;
+    private Integer authorId;
     private String title;
     private Integer priceOld;
     private Integer price;
@@ -12,7 +12,7 @@ public class Book {
     public String toString() {
         return "Book{" +
                 "id=" + id +
-                ", author='" + author + '\'' +
+                ", author='" + authorId + '\'' +
                 ", title='" + title + '\'' +
                 ", priceOld=" + priceOld +
                 ", price=" + price +
@@ -27,12 +27,12 @@ public class Book {
         this.id = id;
     }
 
-    public String getAuthor() {
-        return author;
+    public Integer getAuthorId() {
+        return authorId;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
     }
 
     public String getTitle() {
--- src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -23,7 +23,7 @@ public class BookService {
         List<Book> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rownum)->{
             Book book = new Book();
             book.setId(rs.getInt("id"));
-            book.setAuthor(rs.getString("author"));
+            book.setAuthorId(rs.getInt("authorId"));
             book.setTitle(rs.getString("title"));
             book.setPriceOld(rs.getInt("priceOld"));
             book.setPrice(rs.getInt("price"));
